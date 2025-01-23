e325a4a6cbf7258140aef542fef40835d4b13c8e
MobeenBaig <mobeenbaig011@gmail.com>
2025-01-23 22:55:40 +0500

    authUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/Home/HomePage"
+import { Toaster } from "react-hot-toast"
 
 
 function App() {
   
   return (
+    <>
     <Routes>
       
       <Route path="/" element = {<HomePage/>} />
@@ -14,6 +16,8 @@ function App() {
       <Route path="/signup" element = {<SignUpPage/>} />
     
     </Routes>
+    <Toaster />
+    </>
   )
     
   
